a54a3e827211fbb5a4f444f1312f8fdcd06e2316
mateolomas <63978399+mateolomas@users.noreply.github.com>
2020-04-19 15:23:25 -0500

    Update 1fibo.c

--- 1fibo.c
+++ 1fibo.c
@@ -1,11 +1,14 @@
 
+/*Crear un arreglo arr de 30 elementos tal que arr[n] = fib(n), siendo fib(n) 
+el enésimo término de la serie de Fibonacci: 1, 1, 2, 3, 5, 8, . . . */
+
 
 #include<stdio.h>
 
 int fibo(int n){
     int arr[30] = {0, 1};
     
-    //int arrlen = sizeof(arr)/sizeof(arr[0]);
+    
     
     for(int i = 0; i <n; ++i){
         if(i < 2){
